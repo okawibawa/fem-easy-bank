8f27c6d68af6fcebaaa7a28d59453a5e6d51712b
Oka Wibawa <okaa.wibawa@gmail.com>
2021-09-04 01:24:21 +0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#menu');
+const overlay = document.querySelector('.overlay');
+const desktopLinks = document.querySelector('.desktop__links');
+
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+    overlay.classList.add('fade-out');
+    overlay.classList.remove('fade-in');
+    desktopLinks.classList.remove('fade-in');
+    desktopLinks.classList.add('fade-out');
+  } else {
+    menu.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+    desktopLinks.classList.remove('fade-out');
+    desktopLinks.classList.add('fade-in');
+  }
+});
