9ff784bdff32e5de783a280e33c969a9fc6aebe2
Oka Wibawa <okaa.wibawa@gmail.com>
2021-09-04 01:57:52 +0800

    removes scroll function on open modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#menu');
 const overlay = document.querySelector('.overlay');
 const desktopLinks = document.querySelector('.desktop__links');
+const body = document.getElementById('body');
 
 menu.addEventListener('click', () => {
   if (menu.classList.contains('open')) {
@@ -9,11 +10,13 @@ menu.addEventListener('click', () => {
     overlay.classList.remove('fade-in');
     desktopLinks.classList.remove('fade-in');
     desktopLinks.classList.add('fade-out');
+    body.classList.remove('no-overflow');
   } else {
     menu.classList.add('open');
     overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
     desktopLinks.classList.remove('fade-out');
     desktopLinks.classList.add('fade-in');
+    body.classList.add('no-overflow');
   }
 });
